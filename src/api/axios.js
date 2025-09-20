316d6e2587a84ca1489abae8e7982037b65b5099
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-09-20 23:03:01 +0530

    Add backend baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-auth-backend-3.onrender.com/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Add JWT automatically
